4d5ab84b79ca112bd7dabad023c701599c827d0e
Pablo M <pablo.150520@gmail.com>
2022-03-25 14:57:12 -0600

    Avance 70%

--- admin/post/deleteCoupon.php
+++ admin/post/deleteCoupon.php
@@ -3,9 +3,9 @@ include_once('../../config/config.php');
 $id = s($_POST['ref'], 'STRING');
 $null = '';
 $stmt = $conn->query("DELETE FROM coupons WHERE id='$id'");
-if($stmt == true){
-    $update = $conn->prepare("UPDATE products SET coupon=? WHERE coupon=?");
-    $update->bind_param('ss', $null, $id);
-    $update->execute();
-}
+    if($stmt == true){
+        $update = $conn->prepare("UPDATE products SET coupon=? WHERE coupon=?");
+        $update->bind_param('ss', $null, $id);
+        $update->execute();
+    }
 ?>
