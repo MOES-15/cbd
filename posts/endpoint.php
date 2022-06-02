95c5079cacef483be6495c24b86a3d8d29bb65de
pablo.moes15 <pablo.moes15@gmail.com>
2022-06-02 14:45:57 -0500

    Ajuste

--- posts/endpoint.php
+++ posts/endpoint.php
@@ -1,24 +1,7 @@
 <?php
 require '../vendor/autoload.php';
 include '../config/config.php';
-date_default_timezone_set("America/Mexico_City");
   MercadoPago\SDK::setAccessToken("TEST-6490919314959474-050219-be40aa3585e520a52bd7c0fc1812b532-260364979");
-  switch($_POST["type"]) {
-      case "payment":
-          $payment = MercadoPago\Payment::find_by_id($_POST["data"]["id"]);
-          break;
-      case "plan":
-          $plan = MercadoPago\Plan::find_by_id($_POST["data"]["id"]);
-          break;
-      case "subscription":
-          $plan = MercadoPago\Subscription::find_by_id($_POST["data"]["id"]);
-          break;
-      case "invoice":
-          $plan = MercadoPago\Invoice::find_by_id($_POST["data"]["id"]);
-          break;
-      case "point_integration_wh":
-          // $_POST contiene la informaciòn relacionada a la notificaciòn.
-          break;
-  }
-$add = $conn->query("INSERT INTO content (content, data) VALUES ('$plan', '$data_2')");
+  $payment = MercadoPago\Payment::find_by_id($_POST["data"]["id"]);
+$add = $conn->query("INSERT INTO content (content, data) VALUES ('$payment', '$data_2')");
 ?>
