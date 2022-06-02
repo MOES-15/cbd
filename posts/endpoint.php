e73043ea24be69259bada86e0e0100a27c694af5
pablo.moes15 <pablo.moes15@gmail.com>
2022-06-02 13:51:36 -0500

    Ajuste

--- posts/endpoint.php
+++ posts/endpoint.php
@@ -1,8 +1,7 @@
 <?php
 include '../config/config.php';
 date_default_timezone_set("America/Mexico_City");
-$body = @file_get_contents('php://input');
-$data = json_decode($body);
+$data = @file_get_contents('php://input');
 $add = $conn->prepare("INSERT INTO content (content) VALUES (?)");
 $add->bind_param('s', $data);
 $add->execute();
