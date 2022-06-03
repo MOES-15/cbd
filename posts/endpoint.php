47d8181dbb4d1ca72ae205c3e0dd94e82a86a85e
pablo.moes15 <pablo.moes15@gmail.com>
2022-06-02 20:14:43 -0500

    Ajuste

--- posts/endpoint.php
+++ posts/endpoint.php
@@ -7,12 +7,13 @@ MercadoPago\SDK::setAccessToken('TEST-6490919314959474-050219-be40aa3585e520a52b
         $payment = MercadoPago\Payment::find_by_id($_GET["id"]);
         $merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
         $id = json_encode($merchant_order);
+        $conn->query("INSERT INTO content (content) VALUES ('$id')");
   }else if($_GET["topic"] == 'merchant_order'){
       $merchant_order = MercadoPago\MerchantOrder::find_by_id($_GET["id"]);
       $id = json_encode($merchant_order);
+      $conn->query("INSERT INTO content (content) VALUES ('$id')");
   }
   // If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
-  $conn->query("INSERT INTO content (content) VALUES ('$id')");
   $get = $conn->query("SELECT * FROM orders WHERE id_order = '$id'");
   $data = $get->fetch_assoc();
 
