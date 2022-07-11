5f00aa4fca7ba99270b09dcaeb55c3f153e2d1a3
pablo.moes15 <pablo.moes15@gmail.com>
2022-07-11 15:32:16 -0500

    Ajustes

--- blog.php
+++ blog.php
@@ -72,26 +72,25 @@
         $row = $get->num_rows;
         if ($row != 0) {
             $data = $get->fetch_all(MYSQLI_ASSOC);
-            $num = count($data);
-            for ($i = 0; $i < $num; $i++) {
+            for ($i = 0; $i < $row; $i++) {
                 if ($i == 0) {
                     echo '<div class="grid grid-cols-2 col-span-4 mx-5 border mb-20 hover:shadow-lg  transition duration-500">
-                    <div class="md:col-span-1 col-span-2 flex items-center justify-center flex-col md:h-auto h-72 md:px-auto px-7">
-                        <a href="post?p_ref=' . $data[$i]['titulo'] . '" class="md:w-8/12 md:text-4xl text-xl font-semibold hover:text-yellow-500">' . $data[$i]['titulo'] . '</a>
-                        <div class="my-3 md:w-8/12">' . $data[$i]['fecha'] . ' | ' . $data[$i]['categoria_1'] . ' - ' . $data[$i]['categoria_2'] . '</div>
-                        <div class="pt-3 w-8/12 md:h-32 md:block hidden">' . substr($data[$i]['contenido'], 0, 300) . '</div>
-                        <div class="md:w-8/12 w-full mt-5 flex justify-end">
-                                <a class="border-2 border-yellow-400 hover:bg-yellow-300 px-5 py-3 text-sm flex items-center" href="post?p_ref=' . $data[$i]['titulo'] . '">LEER POST 
-                                    <div class="ml-1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                                        </svg>
+                            <div class="md:col-span-1 col-span-2 flex items-center justify-center flex-col md:h-auto h-72 md:px-auto px-7">
+                                <a href="post?p_ref=' . $data[$i]['titulo'] . '" class="md:w-8/12 md:text-4xl text-xl font-semibold hover:text-yellow-500">' . $data[$i]['titulo'] . '</a>
+                                <div class="my-3 md:w-8/12">' . $data[$i]['fecha'] . ' | ' . $data[$i]['categoria_1'] . ' - ' . $data[$i]['categoria_2'] . '</div>
+                                <div class="pt-3 w-8/12 md:h-32 md:block hidden">' . substr($data[$i]['contenido'], 0, 300) . '</div>
+                                <div class="md:w-8/12 w-full mt-5 flex justify-end">
+                                        <a class="border-2 border-yellow-400 hover:bg-yellow-300 px-5 py-3 text-sm flex items-center" href="post?p_ref=' . $data[$i]['titulo'] . '">LEER POST 
+                                            <div class="ml-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
+                                                </svg>
+                                            </div>
+                                        </a>
                                     </div>
-                                </a>
                             </div>
-                    </div>
-                    <div class="md:col-span-1 col-span-2"><img src="assets/media/img/post/' . $data[$i]['imagen'] . '" alt="" class=""></div>
-                </div>';
+                            <div class="md:col-span-1 col-span-2"><img src="assets/media/img/post/' . $data[$i]['imagen'] . '" alt="" class=""></div>
+                        </div>';
                 } else if ($i == 1) {
                     echo '
                     <div class="grid grid-cols-2 col-span-4 mx-5 border mb-20 hover:shadow-lg  transition duration-500">
@@ -111,7 +110,7 @@
                             </div>
                     </div>
                 </div>';
-                } else {
+                } else if ($i >= 2){
                     echo '<div class="md:col-span-1 col-span-4 mx-5 border mb-10 hover:shadow-lg  transition duration-500">
                             <div class="h-64"><img src="assets/media/img/post/' . $data[$i]['imagen'] . '" alt=""></div>
                             <div class="h-96 px-7">
